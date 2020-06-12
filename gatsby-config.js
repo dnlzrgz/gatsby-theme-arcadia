dddf76adbea36635c84ea19405c503c60fa95402
Danielkvist <danielkvist@protonmail.com>
2020-06-12 12:09:23 +0200

    refactor(gatsby): update title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Arcadia Starter",
-    description: "",
+    title: "Gatsby Arcadia",
+    description: "A modern and awesome starter for Gatsby.",
     author: "@danielkvist",
   },
   plugins: [
