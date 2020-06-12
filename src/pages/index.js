14a02b159149c61904ab9cf788b23d414681d11a
Danielkvist <danielkvist@protonmail.com>
2020-06-12 12:11:57 +0200

    refactor(app): clean up unnecessary code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,12 @@
 import React from "react"
-import styled from "styled-components"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Title = styled.h1`
-  text-align: center;
-`
-
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <Layout>
       <SEO title="Home" />
-      <Title>{data.site.siteMetadata.title}</Title>
-      <p>lorem ipsum </p>
     </Layout>
   )
 }
