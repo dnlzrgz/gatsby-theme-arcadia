0aea7442055bbb2e8230f17a3597530d8fbc775e
Danielkvist <danielkvist@protonmail.com>
2020-06-17 21:53:32 +0200

    refactor(app): remove hover effect

--- src/components/hero-background.js
+++ src/components/hero-background.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react"
+import React, { useRef } from "react"
 import { Canvas, useFrame } from "react-three-fiber"
 import * as THREE from "three"
 
@@ -10,12 +10,7 @@ const Planet = props => {
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={props.hovered ? [1.05, 1.05, 1.05] : [1, 1, 1]}
-      castShadow
-    >
+    <mesh {...props} ref={mesh} scale={[1, 1, 1]} castShadow>
       <icosahedronBufferGeometry attach="geometry" />
       <meshPhongMaterial attach="material" color="white" flatShading />
     </mesh>
@@ -28,12 +23,7 @@ const Skeleton = props => {
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y -= 0.01))
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={props.hovered ? [1.4, 1.4, 1.4] : [1.2, 1.2, 1.2]}
-      castShadow
-    >
+    <mesh {...props} ref={mesh} scale={[1.2, 1.2, 1.2]} castShadow>
       <icosahedronBufferGeometry attach="geometry" />
       <meshPhongMaterial
         attach="material"
@@ -51,12 +41,7 @@ const Star = props => {
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y -= 0.03))
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={props.hovered ? [0.06, 0.06, 0.06] : [0.04, 0.04, 0.04]}
-      castShadow
-    >
+    <mesh {...props} ref={mesh} scale={[0.04, 0.04, 0.04]} castShadow>
       <tetrahedronBufferGeometry attach="geometry" args={[2, 0]} />
       <meshPhongMaterial
         attach="material"
@@ -69,15 +54,8 @@ const Star = props => {
 }
 
 const HeroBackground = () => {
-  const [hover, setHover] = useState(false)
-
   return (
-    <Canvas
-      gl={{ antialias: true, alpha: true }}
-      onMouseEnter={e => setHover(true)}
-      onMouseOut={e => setHover(false)}
-      onBlur={e => setHover(false)}
-    >
+    <Canvas gl={{ antialias: true, alpha: true }}>
       <ambientLight color={Theme.colors.darkGray} intensity={1.0} />
       <directionalLight color={0x999999} position={[1, 0, 0]} />
       <directionalLight color={Theme.colors.main} position={[0.75, 1, 0.5]} />
@@ -85,24 +63,12 @@ const HeroBackground = () => {
         color={Theme.colors.accent}
         position={[-0.75, -1, 0.5]}
       />
-      <Planet position={[0, 0, 0, 0]} hovered={!hover} />
-      <Skeleton position={[0, 0, 0, 0]} hovered={!hover} />
-      <Star
-        position={!hover ? [1.2, 1.2, 1.2, 1.2] : [1, 1, 1, 1]}
-        hovered={!hover}
-      />
-      <Star
-        position={!hover ? [1.2, -1.2, 1.2, -1.2] : [1, -1, 1, -1]}
-        hovered={!hover}
-      />
-      <Star
-        position={!hover ? [-1.2, 1.2, 1.2, 1.2] : [-1, 1, 1, 1]}
-        hovered={hover}
-      />
-      <Star
-        position={!hover ? [-1.2, -1.2, 1.2, -1.2] : [-1, -1, 1, -1]}
-        hovered={!hover}
-      />
+      <Planet position={[0, 0, 0, 0]} />
+      <Skeleton position={[0, 0, 0, 0]} />
+      <Star position={[1, 1, 1, 1]} />
+      <Star position={[1, -1, 1, -1]} />
+      <Star position={[-1, 1, 1, 1]} />
+      <Star position={[-1, -1, 1, -1]} />
     </Canvas>
   )
 }
