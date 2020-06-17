8f159e4de891bf7b30279c3a5926595d39c46daf
Danielkvist <danielkvist@protonmail.com>
2020-06-17 21:54:41 +0200

    refactor(theme): add styles for h2

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   :root {
+    background-color: ${props => props.theme.colors.white};
     color: ${props => props.theme.colors.darkGray};
     font-family: ${props => props.theme.fonts.default};
     font-size: 16px;
@@ -19,14 +20,23 @@ const GlobalStyle = createGlobalStyle`
   h4 {
     font-family: ${props => props.theme.fonts.title};
     line-height: ${props => props.theme.lineHeights.title};
+    margin: 0;
   }
 
   h1 {
-    font-size: ${props => props.theme.fontSizes[16]};
+    font-size: ${props => props.theme.fontSizes[15]};
     font-weight: ${props => props.theme.fontWeight.title};
 
     @media (max-width: ${props => props.theme.mediaQueries.md}) {
       font-size: ${props => props.theme.fontSizes[8]};
+    }
+  }
+
+  h2 {
+    font-size: ${props => props.theme.fontSizes[10]};
+
+    @media (max-width: ${props => props.theme.mediaQueries.md}) {
+      font-size: ${props => props.theme.fontSizes[7]};
     }
   }
 `
